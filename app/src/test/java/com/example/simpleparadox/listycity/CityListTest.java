9861866121e48d4147a8fcfee3ad7d3fad674b1f
Furmaan Sekhon <sekhon54321@gmail.com>
2021-03-21 21:56:24 -0600

    Pass Test

--- app/src/test/java/com/example/simpleparadox/listycity/CityListTest.java
+++ app/src/test/java/com/example/simpleparadox/listycity/CityListTest.java
@@ -22,6 +22,7 @@ class CityListTest {
         CityList cityList = mockCityList();
 
         assertEquals(1, cityList.countCities());
+        cityList.add(new City("Regina", "Saskatchewan"));
 
         assertEquals(2, cityList.countCities());
         assertTrue(cityList.hasCity(new City("Regina", "Saskatchewan")));
